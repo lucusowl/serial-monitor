e188b809588a634072900a7c9adbfa7a9037d740
lucusowl <lucusowl@gmail.com>
2025-04-24 21:04:52 +0900

    feat: v0.2.1 update router
    - add: flush at data read
    - add: error handling
    - add: init protocol when router ready
    - update: serial cancel_read before close
    - update: alert message at invalid protocol

--- backend_app/router.py
+++ backend_app/router.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import threading
+import traceback
 from serial import Serial
 from serial.tools import list_ports
 
@@ -24,34 +25,46 @@ def list_ports_cmd():
 
 def main():
     ser:Serial = None
+    reader_thread = None
 
     def stdin_loop():
         nonlocal ser
+        nonlocal reader_thread
         for line in sys.stdin:
             cmd = json.loads(line)
-            if cmd['CMD'] == 'LIST':
-                list_ports_cmd()
-            elif cmd['CMD'] == 'OPEN':
-                ser = Serial(cmd['PORT'], cmd['BAUD'], timeout=1)
-                print(json.dumps({"EVENT":"OPENED", "PORT":cmd['PORT']}), flush=True)
-                threading.Thread(target=reader, daemon=True).start()
-            elif cmd['CMD'] == 'CLOSE':
-                ser.close()
-                print(json.dumps({"EVENT":"CLOSED"}), flush=True)
-            elif cmd['CMD'] == 'WRITE':
-                # ser.write(bytes.fromhex(cmd['DATA']))
-                ser.write(cmd['DATA'].encode('utf-8'))
-            else:
-                print(json.dumps({"EVENT":"ALERT","MESSAGE":"Invalid Protocol"}), flush=True)
+            try:
+                if cmd['CMD'] == 'LIST':
+                    list_ports_cmd()
+                elif cmd['CMD'] == 'OPEN':
+                    ser = Serial(cmd['PORT'], cmd['BAUD'], timeout=1)
+                    reader_thread = threading.Thread(target=reader, daemon=True)
+                    reader_thread.start()
+                    print(json.dumps({"EVENT":"OPENED", "PORT":cmd['PORT']}), flush=True)
+                elif cmd['CMD'] == 'CLOSE':
+                    # reader 종료
+                    ser.cancel_read()
+                    ser.close()
+                    print(json.dumps({"EVENT":"CLOSED"}), flush=True)
+                elif cmd['CMD'] == 'WRITE':
+                    # ser.write(bytes.fromhex(cmd['DATA']))
+                    ser.write(cmd['DATA'].encode('utf-8'))
+                else:
+                    print(json.dumps({"EVENT":"ALERT","MESSAGE":"Invalid Protocol Detected"}), flush=True)
+            except Exception as e:
+                print(json.dumps({"EVENT":"ERROR","MESSAGE":str(e),"TRACEBACK":traceback.format_exc()}), flush=True, file=sys.stderr)
 
     def reader():
         while ser and ser.is_open:
-            data = ser.read(ser.in_waiting or 1)
-            if data:
-                print(json.dumps({"EVENT":"DATA","DATA":data.hex()}))
+            try:
+                data = ser.read(ser.in_waiting or 1)
+                if data:
+                    print(json.dumps({"EVENT":"DATA","DATA":data.hex()}), flush=True)
+            except Exception as e:
+                print(json.dumps({"EVENT":"ERROR","MESSAGE":str(e),"TRACEBACK":traceback.format_exc()}), flush=True, file=sys.stderr)
 
     stdin_thread = threading.Thread(target=stdin_loop, daemon=True)
     stdin_thread.start()
+    print(json.dumps({"EVENT":"INIT"}), flush=True)
     stdin_thread.join()
 
 if "__main__" == __name__:
